8ea5942cbdfa076ff7751f2b65bbb50eb707144f
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-11-13 00:51:11 -0500

    Create simple tests on simple function

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 import os
+import warnings
 
 import aws_cdk as cdk
 
--- tests/unit/test_campus_events_cloudformation_stack.py
+++ tests/unit/test_campus_events_cloudformation_stack.py
@@ -1,15 +1,17 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from campus_events_cloudformation.campus_events_cloudformation_stack import CampusEventsCloudformationStack
+from campus_events_cloudformation.campus_events_cloudformation_stack import (
+    CampusEventsCloudformationStack,
+)
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in campus_events_cloudformation/campus_events_cloudformation_stack.py
-def test_sqs_queue_created():
+
+def test_lambda_created():
     app = core.App()
     stack = CampusEventsCloudformationStack(app, "campus-events-cloudformation")
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    template.has_resource_properties(
+        "AWS::Lambda::Function",
+        {"Handler": "hello_world.hello_world_handler", "Runtime": "python3.12"},
+    )
