84287d27925d899abf63f3042a6e2fec23a54f1b
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-11-16 16:51:14 -0500

    Lamnda to handle insert of data for us

--- campus_events_cloudformation/campus_events_cloudformation_stack.py
+++ campus_events_cloudformation/campus_events_cloudformation_stack.py
@@ -7,10 +7,10 @@ class CampusEventsCloudformationStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        hello_world_function = _lambda.Function(
+        fetch_rss_feed_fn = _lambda.Function(
             self,
-            "HelloWorldFunction",
-            runtime=_lambda.Runtime.PYTHON_3_12,
-            handler="hello_world.hello_world_handler",
+            "UpdateDataFromRSSFeedFn",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="fetch_new_events.lambda_handler",
             code=_lambda.Code.from_asset("lib"),
         )
