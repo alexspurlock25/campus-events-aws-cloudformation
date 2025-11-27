de594612c989f361819b483cbec584e5c9bd3619
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-11-27 10:31:40 -0500

    Update model to be dataclass

--- lib/Event.py
+++ lib/Event.py
@@ -1,27 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Event:
-    def __init__(
-        self,
-        title: str,
-        host: str,
-        start_date: str,
-        end_date: str,
-        start_time: str,
-        end_time: str,
-        event_description: str,
-        location: str,
-        link: str,
-        categories: list[str],
-    ) -> None:
-        self.title: str = title
-        self.host: str = host
-        self.start_date: str = start_date
-        self.end_date: str = end_date
-        self.start_time: str = start_time
-        self.end_time: str = end_time
-        self.event_description: str = event_description
-        self.location: str = location
-        self.link: str = link
-        self.categories: list[str] = categories
+    title: str
+    host: str
+    start_date: str
+    end_date: str
+    start_time: str
+    end_time: str
+    event_description: str
+    location: str
+    link: str
+    categories: list[str]
 
     def to_dict(self):
         return {
