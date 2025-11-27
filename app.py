bbf81c05648c68ef5981377a84ffd6d2d1a9fb14
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-11-27 10:30:43 -0500

    Remove stack: prep for a better structure

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python3
 # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 import os
-import warnings
 
 import aws_cdk as cdk
 
-from campus_events_cloudformation.campus_events_cloudformation_stack import (
-    CampusEventsCloudformationStack,
-)
-
 app = cdk.App()
-CampusEventsCloudformationStack(
-    app,
-    "CampusEventsCloudformationStack",
-    env=cdk.Environment(
-        account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")
-    ),
-)
 
 app.synth()
