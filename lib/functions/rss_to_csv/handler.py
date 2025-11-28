5ea604f2980807d425901dc02a8b614b96249a97
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-11-28 17:44:08 -0500

    Move Event dataclass
    This is just a simple thing to reduce headache from importing

--- lib/functions/rss_to_csv/handler.py
+++ lib/functions/rss_to_csv/handler.py
@@ -13,7 +13,35 @@ from aws_cdk import aws_s3 as s3
 import boto3
 import feedparser
 from bs4 import BeautifulSoup
-from Event import Event
+
+
+@dataclass
+class Event:
+    title: str
+    host: str
+    start_date: str
+    end_date: str
+    start_time: str
+    end_time: str
+    event_description: str
+    location: str
+    link: str
+    categories: list[str]
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "host": self.host,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+            "event_description": self.event_description,
+            "location": self.location,
+            "external_link": self.link,
+            "categories": self.categories,
+        }
+
 
 date_pattern = r"(\d{1,2}) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (\d{4})"
 time_pattern = r"(\d{2}:\d{2}:\d{2})"
