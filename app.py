0e9a2d7c355ebd79dd0f715b9352d6add0e10065
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-11-28 17:45:03 -0500

    Zed reformatted this too

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-from aws_cdk import App, Environment, Tags
-from lib.config import load_config
-from lib.stacks import S3CSVStack, LambdaStack
 import os
+
+from aws_cdk import App, Environment, Tags
+
+from lib.config import load_config
+from lib.stacks import LambdaStack, S3CSVStack
 
 app = App()
 
-env_name = app.node.try_get_context("environment") or "prod" # should be dev in the real world
+env_name = (
+    app.node.try_get_context("environment") or "prod"
+)  # should be dev in the real world
 config = load_config(environment=env_name)
 aws_env = Environment(
     account=os.environ.get("CDK_DEFAULT_ACCOUNT"),
-    region=os.environ.get("CDK_DEFAULT_REGION", "us-east-2")
+    region=os.environ.get("CDK_DEFAULT_REGION", "us-east-2"),
 )
 
 s3_csv_stack = S3CSVStack(
     scope=app,
     id=f"RssPipeline-Storage-{config.environment}",
     config=config,
-    env=aws_env
+    env=aws_env,
 )
 
 lambda_stack = LambdaStack(
@@ -26,7 +30,7 @@ lambda_stack = LambdaStack(
     id=f"RssPipeline-Lambda-{config.environment}",
     config=config,
     csv_bucket=s3_csv_stack.csv_bucket,
-    env=aws_env
+    env=aws_env,
 )
 lambda_stack.add_dependency(s3_csv_stack)
 
