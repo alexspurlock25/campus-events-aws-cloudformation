190250a7fcd5500a7ed5ef998862f43a55efb597
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-11-27 22:13:20 -0500

    Added an event watch rule to the function

--- lib/stacks/lamnda_stack.py
+++ lib/stacks/lamnda_stack.py
@@ -1,5 +1,10 @@
-from typing import Any, Dict, Mapping, Sequence
-from aws_cdk import Stack, aws_lambda, aws_s3
+from aws_cdk import (
+    Stack, 
+    aws_lambda as lambda_, 
+    aws_s3 as s3,
+    aws_events as events,
+    aws_events_targets as targets
+)
 from constructs import Construct
 from lib.config import PipelineConfig
 
@@ -8,15 +13,17 @@ class LambdaStack(Stack):
     Lambda stack handle the rss to csv handler
     '''
 
-    def __init__(self, scope: Construct, id: str, csv_bucket: aws_s3.Bucket, config: PipelineConfig, **kwargs) -> None:
+    rss_function: lambda_.Function
+
+    def __init__(self, scope: Construct, id: str, csv_bucket: s3.Bucket, config: PipelineConfig, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        function = aws_lambda.Function(
+        function = lambda_.Function(
             scope=self,
             id=f"RssFunction-{config.rss_feed.name}",
-            runtime=aws_lambda.Runtime.PYTHON_3_14,
+            runtime=lambda_.Runtime.PYTHON_3_14,
             handler="handler.lambda_handler",
-            code=aws_lambda.Code.from_asset("lib/functions/rss_to_csv"),
+            code=lambda_.Code.from_asset("lib/functions/rss_to_csv"),
             environment={
                 "RSS_FEED_URL": config.rss_feed.url,
                 "RSS_FEED_NAME": config.rss_feed.name,
@@ -24,3 +31,17 @@ class LambdaStack(Stack):
                 "ENVIRONMENT": config.environment
             }
         )
+
+        csv_bucket.grant_write(function)
+
+        rule = events.Rule(
+            self, 
+            f"RssSchedule={config.rss_feed.name}",
+            schedule=events.Schedule.expression(config.rss_feed.schedule_expression)
+        )
+
+        rule.add_target(
+            targets.LambdaFunction(function)
+        )
+
+        self.rss_function = function
