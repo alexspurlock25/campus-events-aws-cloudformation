8256e75040a2bef0fad07b04f2bc9ff85a734ebe
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-12-14 15:07:44 -0500

    move code from original impl and fetch all files in raw stage

--- lib/scripts/transform_xml_to_csv.py
+++ lib/scripts/transform_xml_to_csv.py
@@ -1,7 +1,13 @@
-import sys
-import logging
+import sys, re, feedparser, csv
+from io import StringIO
+from dataclasses import fields, asdict
+from datetime import datetime
 from dataclasses import dataclass
 from awsglue.utils import getResolvedOptions
+from bs4 import BeautifulSoup
+import boto3
+
+s3_client = boto3.client("s3")
 
 
 @dataclass
@@ -30,5 +36,136 @@ args = Args(
     target_bucket_name=_args["TARGET_BUCKET_NAME"],
 )
 
-print("print: Job job started!")
-print(f"print: job args: {args}")
+date_pattern = r"(\d{1,2}) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (\d{4})"
+time_pattern = r"(\d{2}:\d{2}:\d{2})"
+
+
+@dataclass
+class Event:
+    event_id: str
+    title: str
+    host: str
+    start_date: str
+    end_date: str
+    start_time: str
+    end_time: str
+    event_description: str
+    location: str
+    link: str
+    categories: list[str]
+
+    def to_dict(self):
+        return {
+            "event_id": self.event_id,
+            "title": self.title,
+            "host": self.host,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+            "event_description": self.event_description,
+            "location": self.location,
+            "external_link": self.link,
+            "categories": self.categories,
+        }
+
+
+def parse_rss(url: str) -> list[Event]:
+    events: list[Event] = []
+    feed = feedparser.parse(url)
+
+    for entry in feed.entries:
+        categories: list[str] = []
+        for tag in entry.tags:
+            categories.append(str(tag.term))
+
+        title = str(entry["title"]).strip()
+        event_id = get_digits_from_guid(guid=str(entry["guid"]).strip())
+        host = ""
+        location = ""
+        link = ""
+        if "host" in entry:
+            host = str(entry["host"]).strip()
+            location = re.sub(r"[^\x00-\x7F]+", " ", str(entry["location"])).strip()
+            link = str(entry["link"]).strip()
+
+        start_date_match = re.search(date_pattern, str(entry["start"]))
+        start_date = ""
+        if start_date_match is not None:
+            start_date = datetime.strptime(
+                start_date_match.group(0), "%d %b %Y"
+            ).strftime("%Y-%m-%d")
+
+        end_date_match = re.search(date_pattern, str(entry["end"]))
+        end_date = ""
+        if end_date_match is not None:
+            end_date = datetime.strptime(end_date_match.group(0), "%d %b %Y").strftime(
+                "%Y-%m-%d"
+            )
+
+        start_time_match = re.search(time_pattern, str(entry["start"]))
+        start_time = ""
+        if start_time_match is not None:
+            start_time = start_time_match.group(0)
+
+        end_time_match = re.search(time_pattern, str(entry["end"]))
+        end_time = ""
+        if end_time_match is not None:
+            end_time = end_time_match.group(0)
+
+        stripped_html = BeautifulSoup(str(entry["summary"]), "html.parser").get_text(
+            strip=True
+        )
+        event_description = re.sub(r"[^\x00-\x7F]+", " ", stripped_html)
+
+        event = Event(
+            event_id,
+            title,
+            host,
+            start_date,
+            end_date,
+            start_time,
+            end_time,
+            event_description,
+            location,
+            link,
+            categories,
+        )
+        events.append(event)
+    return events
+
+
+def get_digits_from_guid(guid: str) -> str:
+    return guid.rsplit("/")[-1]
+
+
+def events_to_csv(events: list[Event]):
+    output = StringIO()
+    field_names = [f.name for f in fields(Event)]
+
+    writer = csv.DictWriter(output, fieldnames=field_names)
+    writer.writeheader()
+
+    for event in events:
+        writer.writerow(asdict(event))
+
+    return output.getvalue()
+
+
+def get_raw_files() -> list[str]:
+    paginator = s3_client.get_paginator("list_objects_v2")
+    files: list[str] = []
+    for page in paginator.paginate(Bucket=args.source_bucket_name):
+        for obj in page.get("Contents", []):
+            file = obj.get("Key")
+            if file and not file.endswith("/"):
+                files.append(file)
+    return files
+
+
+def main():
+    print(get_raw_files())
+
+
+if __name__ == "__main__":
+    main()
