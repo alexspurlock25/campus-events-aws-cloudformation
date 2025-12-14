a41b835e74c133af09c22d5e6c5ab1a3d3063171
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-12-14 14:02:52 -0500

    update glue version and provide args to script

--- lib/stacks/raw_to_csv_glue_job_stack.py
+++ lib/stacks/raw_to_csv_glue_job_stack.py
@@ -20,6 +20,11 @@ class RawToCsvGlueJobStack(Stack):
             scope=self,
             id=f"{construct_id}-role",
             assumed_by=iam.ServicePrincipal("glue.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "service-role/AWSGlueServiceRole"
+                )
+            ],
         )
 
         props.scripts_bucket.grant_read(glue_rule)
@@ -28,9 +33,19 @@ class RawToCsvGlueJobStack(Stack):
             scope=self,
             id=f"{construct_id}-job",
             role=glue_rule.role_arn,
+            glue_version="5.0",
+            worker_type="G.1X",
+            number_of_workers=2,
             command=glue.CfnJob.JobCommandProperty(
                 name="glueetl",
                 python_version="3",
                 script_location=f"s3://{props.scripts_bucket.bucket_name}/scripts/transform_xml_to_csv.py",
             ),
+            default_arguments={
+                "--enable-spark-ui": True,
+                "--enable-metrics": True,
+                "--enable-continuous-cloudwatch-log": True,
+                "--SOURCE_BUCKET_NAME": props.raw_bucket.bucket_name,
+                "--TARGET_BUCKET_NAME": props.staging_bucket.bucket_name,
+            },
         )
