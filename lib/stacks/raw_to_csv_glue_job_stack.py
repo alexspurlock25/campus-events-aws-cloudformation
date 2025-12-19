977318c0a2e8782b3b99a1dd106c69b0d4bad9fe
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-12-19 01:14:09 -0500

    create an event rule to list for glue job finishing

--- lib/stacks/raw_to_csv_glue_job_stack.py
+++ lib/stacks/raw_to_csv_glue_job_stack.py
@@ -90,35 +90,6 @@ class RawToCsvGlueJobStack(Stack):
         )
         props.staging_bucket.grant_read(crawler_role)
 
-        athena.CfnWorkGroup(
-            scope=self,
-            id=f"{construct_id}-workgroup",
-            name=f"{construct_id}-workgroup",
-            state="ENABLED",
-            work_group_configuration=athena.CfnWorkGroup.WorkGroupConfigurationProperty(
-                enforce_work_group_configuration=True,
-                result_configuration=athena.CfnWorkGroup.ResultConfigurationProperty(
-                    output_location=f"s3://{props.athena_results_bucket.bucket_name}/athena-results/"
-                ),
-            ),
-        )
-
-        glue_crawler = glue.CfnCrawler(
-            scope=self,
-            id=f"{construct_id}-stage-crawler",
-            role=crawler_role.role_arn,
-            database_name=glue_db_name,
-            targets=glue.CfnCrawler.TargetsProperty(
-                s3_targets=[
-                    glue.CfnCrawler.S3TargetProperty(
-                        path=f"s3://{props.staging_bucket.bucket_name}/"
-                    )
-                ]
-            ),
-            table_prefix="events_",
-        )
-        glue_crawler.add_dependency(glue_db)
-
         rule = events.Rule(
             scope=self,
             id="CampusEventsOnRawPutEventRule",
@@ -159,6 +130,70 @@ class RawToCsvGlueJobStack(Stack):
                     resources=[
                         f"arn:aws:glue:{Aws.REGION}:{Aws.ACCOUNT_ID}:job/{glue_job_name}"
                     ],
+                ),
+            )
+        )
+
+        athena.CfnWorkGroup(
+            scope=self,
+            id="CampusEventsWorkgroup",
+            name=f"{construct_id}-workgroup",
+            state="ENABLED",
+            work_group_configuration=athena.CfnWorkGroup.WorkGroupConfigurationProperty(
+                enforce_work_group_configuration=True,
+                result_configuration=athena.CfnWorkGroup.ResultConfigurationProperty(
+                    output_location=f"s3://{props.athena_results_bucket.bucket_name}/athena-results/"
+                ),
+            ),
+        )
+
+        glue_crawler = glue.CfnCrawler(
+            scope=self,
+            id="CampusEventsStagingBucketCrawler",
+            name=f"{construct_id}-stage-crawler",
+            role=crawler_role.role_arn,
+            database_name=glue_db_name,
+            targets=glue.CfnCrawler.TargetsProperty(
+                s3_targets=[
+                    glue.CfnCrawler.S3TargetProperty(
+                        path=f"s3://{props.staging_bucket.bucket_name}/"
+                    )
+                ]
+            ),
+            table_prefix="events_",
+        )
+        glue_crawler.add_dependency(glue_db)
+
+        glue_crawler_success_rule = events.Rule(
+            scope=self,
+            id="CampusEventsConvertToCsvJobSucceededRule",
+            rule_name=f"{construct_id}-job-succeeded-rule",
+            event_pattern=events.EventPattern(
+                source=["aws.glue"],
+                detail_type=["Glue Job State Change"],
+                detail={"jobName": [job.ref], "state": ["SUCCEEDED"]},
+            ),
+        )
+        glue_crawler_success_rule.node.add_dependency(job)
+
+        crawler_arn = (
+            f"arn:aws:glue:{Aws.REGION}:{Aws.ACCOUNT_ID}:crawler/{glue_crawler.name}"
+        )
+        eventbridge_role.add_to_policy(
+            iam.PolicyStatement(
+                actions=["glue:StartCrawler"],
+                resources=[crawler_arn],
+            )
+        )
+
+        glue_crawler_success_rule.add_target(
+            targets.AwsApi(
+                service="Glue",
+                action="startCrawler",
+                parameters={"Name": glue_crawler.name},
+                policy_statement=iam.PolicyStatement(
+                    actions=["glue:StartCrawler"],
+                    resources=[crawler_arn],
                 ),
             )
         )
