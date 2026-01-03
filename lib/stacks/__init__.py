eb5d04eb6ee0efc9d1241d28b4b933b2eff46458
Alexander Spurlock <alexanderspurlock@gmail.com>
2026-01-02 21:54:20 -0500

    update dynamodb stack
    create rule to listen for staging changes create lambda function to
    process new csv file into table

--- lib/stacks/__init__.py
+++ lib/stacks/__init__.py
@@ -1,9 +1,16 @@
-from .rss_lambda_stack import RssToCsvLambdaStack
-from .s3_csv_stack import S3CSVStack
-from .dynamodb_stack import DynamoDBStack
+from .dynamodb_stack import DynamoDBStack, DynamoDBStackParamProps
 from .raw_to_csv_glue_job_stack import (
     RawToCsvGlueJobStack,
     RawToCsvGlueJobStackParamProps,
 )
+from .rss_lambda_stack import RssToCsvLambdaStack
+from .s3_csv_stack import S3CSVStack
 
-__all__ = ["S3CSVStack", "RssToCsvLambdaStack", "DynamoDBStack", "RawToCsvGlueJobStack"]
+__all__ = [
+    "S3CSVStack",
+    "RssToCsvLambdaStack",
+    "DynamoDBStack",
+    "RawToCsvGlueJobStack",
+    "RawToCsvGlueJobStackParamProps",
+    "DynamoDBStackParamProps",
+]
--- lib/stacks/dynamodb_stack.py
+++ lib/stacks/dynamodb_stack.py
@@ -2,7 +2,27 @@
 Docstring here
 """
 
-from aws_cdk import RemovalPolicy, Stack, aws_dynamodb
+import os
+from dataclasses import dataclass
+
+from aws_cdk import (
+    Duration,
+    RemovalPolicy,
+    Stack,
+    aws_dynamodb,
+    aws_lambda,
+    aws_s3,
+    aws_s3_notifications,
+)
+from aws_cdk import aws_events as events
+from aws_cdk import aws_events_targets as targets
+from aws_cdk.aws_lambda import Code, Runtime
+from aws_cdk.aws_s3 import IBucket
+
+
+@dataclass
+class DynamoDBStackParamProps:
+    staging_bucket: IBucket
 
 
 class DynamoDBStack(Stack):
@@ -10,8 +30,10 @@ class DynamoDBStack(Stack):
     Docstring here
     """
 
-    def __init__(self, scope, construct_id) -> None:
-        super().__init__(scope, construct_id)
+    def __init__(
+        self, scope, construct_id, props: DynamoDBStackParamProps, **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
 
         events_table = aws_dynamodb.Table(
             scope=self,
@@ -22,6 +44,7 @@ class DynamoDBStack(Stack):
             ),
             billing_mode=aws_dynamodb.BillingMode.PAY_PER_REQUEST,
             removal_policy=RemovalPolicy.DESTROY,
+            point_in_time_recovery=False,
         )
 
         events_table.add_global_secondary_index(
@@ -33,3 +56,41 @@ class DynamoDBStack(Stack):
                 name="start_time", type=aws_dynamodb.AttributeType.STRING
             ),
         )
+
+        lambda_dir = os.path.join("lib", "functions", "csv_to_dynamo_table")
+
+        loader_lambda = aws_lambda.Function(
+            scope=self,
+            id="CampusEventsCsvLambdaLoader",
+            function_name=f"{construct_id}-csv-to-table",
+            runtime=Runtime.PYTHON_3_14,
+            code=Code.from_asset(path=lambda_dir),
+            handler="csv_to_dynamo_table_fn.handler",
+            timeout=Duration.minutes(5),
+            memory_size=256,  # Sufficient for CSV parsing
+            environment={
+                "TABLE_NAME": events_table.table_name,
+            },
+        )
+        events_table.grant_write_data(loader_lambda)
+
+        # Create EventBridge rule
+        rule = events.Rule(
+            scope=self,
+            id="CsvUploadInStagingBucketRule",
+            rule_name=f"{construct_id}-on-staging-obj-created",
+            event_pattern=events.EventPattern(
+                source=["aws.s3"],
+                detail_type=["Object Created"],
+                detail={
+                    "bucket": {"name": [props.staging_bucket.bucket_name]},
+                    "object": {"key": [{"suffix": ".csv"}]},
+                },
+            ),
+        )
+
+        # Add Lambda as target
+        rule.add_target(targets.LambdaFunction(loader_lambda))
+
+        # Grant Lambda read permissions on the bucket
+        props.staging_bucket.grant_read(loader_lambda)
