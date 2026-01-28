6e10c3d252bb429b4c3c35a99cd79e175364672a
Alexander Spurlock <alexanderspurlock@gmail.com>
2026-01-28 09:58:36 -0500

    Create Athena workgroup

--- lib/infrastructure/stacks/analytics_resources_stack.py
+++ lib/infrastructure/stacks/analytics_resources_stack.py
@@ -3,6 +3,7 @@ Analytics infrastructure stack.
 Manages S3 bucket for Athena query results and analytics outputs.
 """
 
+from aws_cdk import aws_athena as athena
 from aws_cdk import RemovalPolicy, Stack
 from aws_cdk import aws_s3 as s3
 from constructs import Construct
@@ -23,9 +24,23 @@ class AnalyticsResourcesStack(Stack):
 
         self.athena_results_bucket = s3.Bucket(
             scope=self,
-            id="AthenaResultsBucket",
+            id="AnalyticsAthenaResults",
             bucket_name=f"{construct_id}-athena-results",
             removal_policy=RemovalPolicy.DESTROY,
             auto_delete_objects=True,
             versioned=True,
+        )
+
+        athena.CfnWorkGroup(
+            scope=self,
+            id="AnalyticsWorkgroup",
+            name=f"{construct_id}-workgroup",
+            state="ENABLED",
+            recursive_delete_option=True,
+            work_group_configuration=athena.CfnWorkGroup.WorkGroupConfigurationProperty(
+                enforce_work_group_configuration=True,
+                result_configuration=athena.CfnWorkGroup.ResultConfigurationProperty(
+                    output_location=f"s3://{self.athena_results_bucket.bucket_name}/athena-results/"
+                ),
+            ),
         )
