270fae9b352222ebf49c7ebce53bf190127eb96b
Alexander Spurlock <alexanderspurlock@gmail.com>
2026-02-21 17:17:04 -0500

    feat: alter the config file to be simpler

--- app.py
+++ app.py
@@ -20,20 +20,8 @@ from lib.pipeline.stacks import (
 
 app = App()
 
-env_name = (
-    app.node.try_get_context("environment")
-    or "prod"  # should be dev if environment value is missing in the real world
-)
-env_config = load_environment_config(environment=env_name)
-project_config = load_projecttoml_config()
-
-if env_config is None:
-    print(f"Configuration for environment '{env_name}' not found.", file=sys.stderr)
-    sys.exit(1)
-
-if project_config is None:
-    print("Something went wrong while reading your pyproject.py file.", file=sys.stderr)
-    sys.exit(1)
+env_config = load_environment_config()
+app_config = load_projecttoml_config()
 
 root_construct_id = "-".join([project_config.project_name, env_config.environment])
 
--- lib/config/__init__.py
+++ lib/config/__init__.py
@@ -1,3 +1,1 @@
-from .config import PipelineConfig, load_environment_config, load_projecttoml_config
-
-__all__ = ["PipelineConfig", "load_environment_config", "load_projecttoml_config"]
+from .config import RssFeedConfig, load_environment_config, load_projecttoml_config
