40ab261c61abb3377d97f1b223eedebc90bd5c6e
Alexander Spurlock <alexanderspurlock@gmail.com>
2026-03-08 18:48:57 +0000

    feat: remove events that were removed from source in dynamo db

--- lib/pipeline/scripts/glue/silver_to_dynamo_events.py
+++ lib/pipeline/scripts/glue/silver_to_dynamo_events.py
@@ -1,15 +1,13 @@
 import sys
+from typing import List
 from dataclasses import dataclass
 
 import boto3
-from awsglue.context import GlueContext
+from awsglue.context import DataFrame, GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
-from pyspark.sql.functions import col
 from uc_types import file_schema
-
-s3_client = boto3.client("s3")
 
 
 @dataclass
@@ -38,18 +36,49 @@ args = Args(
     dynamo_table_name=_args["DYNAMO_TABLE"],
 )
 
+s3_client = boto3.client("s3")
+dynamodb = boto3.resource("dynamodb")
+dynamo_table = dynamodb.Table(args.dynamo_table_name)
+
 sc = SparkContext()
 glue_context = GlueContext(sc)
 spark = glue_context.spark_session
 
 
+def current_dynamodb_data() -> DynamicFrame:
+    """
+    Reads the DynamoDB table and returns a DynamicFrame
+    """
+
+    dyf = glue_context.create_dynamic_frame.from_options(
+        connection_type="dynamodb",
+        connection_options={
+            "dynamodb.input.tableName": args.dynamo_table_name,
+            "dynamodb.throughput.read.percent": "1.0",
+        },
+    )
+    return dyf
+
+
 def main():
     try:
-        df = spark.read.schema(file_schema()).parquet(
+        new_df = spark.read.schema(file_schema()).parquet(
             f"s3://{args.silver_bucket_name}/uc_events/"
         )
+        current_dynamodb_df = current_dynamodb_data().toDF()
 
-        dyf = DynamicFrame.fromDF(df, glue_context, "events_dyf")
+        removed_events_df = current_dynamodb_df.join(
+            new_df, on="event_id", how="left_anti"
+        )
+
+        print(f"Deleted events '{removed_events_df.count()}':")
+        removed_events_df.show()
+
+        with dynamo_table.batch_writer() as batch:
+            for row in removed_events_df.collect():
+                batch.delete_item(Key={"event_id": row.event_id})
+
+        dyf = DynamicFrame.fromDF(new_df, glue_context, "events_dyf")
 
         glue_context.write_dynamic_frame.from_options(
             frame=dyf,
@@ -59,6 +88,7 @@ def main():
                 "dynamodb.throughput.write.percent": "1.0",
             },
         )
+
     except Exception as e:
         print(f"Error: {e}")
         raise
